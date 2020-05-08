841fb5dfad45e7d6712c1b912146362b8425374b
SaeedaMansoor <60513170+SaeedaMansoor@users.noreply.github.com>
2020-05-08 05:12:24 +0500

    Add files via upload

---
+++ Dictionarys.py
@@ -0,0 +1,23 @@
+# dictionary arelike the mini database with no ordered elements
+# there are labels of elements in a dictionary
+# the example of dictionary is just like a bag with unordered books
+o=dict(); # o is an object of dictionary
+o['money']=12;
+o['price']=14;
+o['number']=2;
+# here money is the label and 12 is the element
+# in short the label of 122 in dictionary is money
+print(o);
+# to  print something out we write or refer to it with the label assigned to it
+# for example
+print(o['money'])  # this will print the value
+print(o['money']+2)  # this will add 2 to the value
+
+
+print()
+print()
+print()
+# you can make an empty dictionary by using the CURLY BRACKETS
+# the format is
+mydictionary={'label1':23,'label2':24}
+print(mydictionary)
